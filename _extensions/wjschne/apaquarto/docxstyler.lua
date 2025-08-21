48c9a3a868407c1c2397af5a215bf621aa907102
Amelia <43177681+rameliaz@users.noreply.github.com>
2025-08-21 19:26:53 +0200

    {{< include _extensions -- is no longer needed with the new version of apaquarto

--- _extensions/wjschne/apaquarto/docxstyler.lua
+++ _extensions/wjschne/apaquarto/docxstyler.lua
@@ -8,23 +8,28 @@ end
 --- Is the class included in the customclasses table?
 --- https://stackoverflow.com/a/2282542/4513316
 function utils_Set(list)
-    local set = {}
-    for _, l in ipairs(list) do set[l] = true end
-    return set
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
 end
 
 -- Classes that are converted. Add additional classes as needed.
-customclasses = { 
-  "Author", 
+customclasses = {
+  "Author",
   "AuthorNote",
-  "Abstract", 
-  "FigureTitle", 
-  "FigureNote", 
-  "FigureWithNote", 
-  "FigureWithoutNote", 
+  "Abstract",
+  "AbstractFirstParagraph",
+  "FigureTitle",
+  "FigureNote",
+  "FigureWithNote",
+  "FigureWithoutNote",
   "Caption",
   "Compact",
-  "NoIndent"
+  "NoIndent",
+  "NextBlockText",
+  "AfterWithoutNote",
+  "H4",
+  "H5"
 }
 
 -- Consult some value
@@ -32,11 +37,14 @@ _set = utils_Set(customclasses)
 
 
 -- https://jmablog.com/post/pandoc-filters/
-function Div (elem)
-    if _set[elem.classes[1]] then
-      elem.attributes['custom-style'] = elem.classes[1]
-      return elem
-    else
-      return elem
-    end
+local function customstyler(elem)
+  if _set[elem.classes[1]] then
+    elem.attributes['custom-style'] = elem.classes[1]
+    return elem
+  end
 end
+
+return {
+  { Span = customstyler },
+  { Div = customstyler }
+}
